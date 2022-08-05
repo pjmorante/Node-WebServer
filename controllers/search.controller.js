148d81d147232609bb8f6ea364fc64e0f40b6e42
Pedro Morante <pedro.morante@outlook.com>
2022-08-04 20:41:53 -0500

    add searching users by id

--- controllers/search.controller.js
+++ controllers/search.controller.js
@@ -1,14 +1,45 @@
 const { response } = require('express');
+const { ObjectId } = require('mongoose').Types;
 
+const { Categoria, Producto } = require('../models');
+const { Usuario } = require('../models/usuario.modelo');
+
+const coleccionesPermitidas = ['usuarios', 'categorias', 'productos', 'roles'];
+
+const buscarUsuarios = async(termino = '', res = response) => {
+
+  const esMongoID = ObjectId.isValid(termino);
+
+  if(esMongoID) {
+    const usuario = await Usuario.findById(termino);
+    res.json({ 
+      results: (usuario) ? [usuario] : []
+    });
+  }
+}
 
 const buscar = (req, res = response) => {
 
   const { coleccion, termino } = req.params;
 
-  res.json({
-    coleccion,
-    termino
-  })
+  if(!coleccionesPermitidas.includes(coleccion)){
+    return res.status(400).json({
+      msg: `Las colecciones permitidas son ${coleccionesPermitidas}`,
+    });
+  }
+
+  switch (coleccion) {
+    case 'usuarios':
+      buscarUsuarios(termino, res);
+      break;
+    case 'categorias':
+      break;
+    case 'productos':
+      break;
+
+    default:
+      res.status(500).json({ msg: 'Upps' });
+  }
 }
 
 

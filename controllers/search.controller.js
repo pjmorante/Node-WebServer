579c18af34e218bbf019881ca7d5e1a00df7694c
Pedro Morante <pedro.morante@outlook.com>
2022-08-04 20:05:39 -0500

    add search route

---
+++ controllers/search.controller.js
@@ -0,0 +1,15 @@
+const { response } = require('express');
+
+
+const buscar = (req, res = response) => {
+
+  const { coleccion, termino } = req.params;
+
+  res.json({
+    coleccion,
+    termino
+  })
+}
+
+
+module.exports = {buscar};

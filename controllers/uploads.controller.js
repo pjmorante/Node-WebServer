fe622eb22e8dfc290693292180c1d874e4906a22
Pedro Morante <pedro.morante@outlook.com>
2022-08-11 19:30:29 -0500

    update uploads controller and verify extensions allowed

--- controllers/uploads.controller.js
+++ controllers/uploads.controller.js
@@ -9,16 +9,29 @@ const cargarArchivo = ( req, res = response ) => {
   }
 
   const { archivo } = req.files;
+  const nombreCortado = archivo.name.split('.');
+  const extension = nombreCortado[nombreCortado.length - 1];
 
-  const uploadPath = path.join(__dirname, '../uploads/', archivo.name);
+  const extensionesValidas = ['png', 'jpg', 'jpeg', 'gif'];
+  if (!extensionesValidas.includes(extension)){
+    return res
+      .status(400)
+      .json({
+        msg: `La extensión ${extension} no es permitida, ${ extensionesValidas }`
+      });
+  }
 
-  archivo.mv(uploadPath, (err) => {
-    if (err) {
-      return res.status(500).json({err});
-    }
+  res.json({msg: `${extension}`});
 
-    res.json({ msg: 'File uploaded to ' + uploadPath });
-  });
+  // const uploadPath = path.join(__dirname, '../uploads/', archivo.name);
+
+  // archivo.mv(uploadPath, (err) => {
+  //   if (err) {
+  //     return res.status(500).json({err});
+  //   }
+
+  //   res.json({ msg: 'File uploaded to ' + uploadPath });
+  // });
 
 }
 
